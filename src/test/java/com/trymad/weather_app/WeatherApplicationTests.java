f228b545796b967031a4b96a251d976d8eb71d24
Kuznesov Ruslan <kusnetsov.ruslan.mkn@gmail.com>
2024-06-17 12:39:21 +0300

    init empty project

---
+++ src/test/java/com/trymad/weather_app/WeatherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trymad.weather_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
